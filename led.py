af9b60cf4b103f465d28fcfe62554e31825d2eae
eryilmazfurkan <mfurkaneryilmaz28@gmail.com>
2024-05-20 23:21:31 +0300

    change led blink

--- led.py
+++ led.py
@@ -14,44 +14,31 @@ bus.write_byte_data(dev_addr, conf_addr, output_data)
 
 # P7 P6 P5 P4 P3 P2 P1 P0
 
-# TURN ON USER LED CONNECTED TO P0
-bus.write_byte_data(dev_addr, write_addr, 0xFE)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P1
+bus.write_byte_data(dev_addr, write_addr, 0x02)
+time.sleep(5)
 bus.write_byte_data(dev_addr, write_addr, 0xFD)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P2
-bus.write_byte_data(dev_addr, write_addr, 0xFB)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P3
-bus.write_byte_data(dev_addr, write_addr, 0xF7)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P4
-bus.write_byte_data(dev_addr, write_addr, 0xEF)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P5
-bus.write_byte_data(dev_addr, write_addr, 0xDF)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P6
-bus.write_byte_data(dev_addr, write_addr, 0xBF)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
-time.sleep(1)
-# TURN ON USER LED CONNECTED TO P7
-bus.write_byte_data(dev_addr, write_addr, 0x7F)
-time.sleep(2)
-bus.write_byte_data(dev_addr, write_addr, 0xFF)
 
+time.sleep(2)
+
+bus.write_byte_data(dev_addr, write_addr, 0x01)
+time.sleep(5)
+bus.write_byte_data(dev_addr, write_addr, 0xFD)
+
+time.sleep(2)
+
+bus.write_byte_data(dev_addr, write_addr, 0xDB)
+time.sleep(5)
+bus.write_byte_data(dev_addr, write_addr, 0xFD)
+
+time.sleep(2)
+
+bus.write_byte_data(dev_addr, write_addr, 0xB4)
+time.sleep(5)
+bus.write_byte_data(dev_addr, write_addr, 0xFD)
+
+time.sleep(2)
+
+bus.write_byte_data(dev_addr, write_addr, 0x6D)
+time.sleep(5)
+bus.write_byte_data(dev_addr, write_addr, 0xFD)
 
